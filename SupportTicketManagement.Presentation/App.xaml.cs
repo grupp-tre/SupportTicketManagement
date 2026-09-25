89f60f25489ca0b8d911d6da0d7c91d1b4a4918f
jackhoffsten <jack.hoffsten@hotmail.se>
2026-09-25 10:58:34 +0200

    Fixes project structure

--- SupportTicketManagement.Presentation/App.xaml.cs
+++ SupportTicketManagement.Presentation/App.xaml.cs
@@ -24,7 +24,7 @@ namespace SupportTicketManagement.Presentation
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    public partial class App : Application
+    public partial class App : Microsoft.UI.Xaml.Application
     {
         private Window? _window;
 
