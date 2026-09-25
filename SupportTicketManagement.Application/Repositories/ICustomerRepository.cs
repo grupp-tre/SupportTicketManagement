23b4e65377eb6cb765a43f2930e593d8a2a51488
jackhoffsten <jack.hoffsten@hotmail.se>
2026-09-25 10:33:45 +0200

    Add repository interfaces

---
+++ SupportTicketManagement.Application/Repositories/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupportTicketManagement.Domain.Models;
+
+namespace SupportTicketManagement.Application.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<bool> Create(Customer customer);
+    Task<IReadOnlyList<Customer>> GetAll();
+    Task<bool> Update(Customer customer);
+}
---
+++ SupportTicketManagement.Application/Repositories/ITicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupportTicketManagement.Domain.Models;
+
+namespace SupportTicketManagement.Application.Repositories;
+
+public interface ITicketRepository
+{
+    Task<bool> Create(Ticket ticket);
+    Task<IReadOnlyList<Ticket>> GetAll();
+    Task<bool> Update(Ticket ticket);
+}
